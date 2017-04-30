808c11498eed0d03f064c2774e40ee66b6ee150c
Alex Vear <axvr@users.noreply.github.com>
2017-04-30 18:40:11 +0100

    Directory restructure for next version of ALIS

---
+++ 2017/alis/old/conf_files/install/edit_pacmanconf.pl
@@ -0,0 +1,13 @@
+#!/bin/perl
+
+# ALIS - Arch Linux Installation Script
+# =====================================
+#
+# The successor to Architect Linux
+#
+# This is the conf_files/install/edit_pacmanconf.pl file for ALIS
+# (https://github.com/axvr/alis).
+# Created by Alex Vear - axvr (https://github.com/axvr).
+#
+# This project is licenced under the MIT Licence
+# (https://github.com/axvr/alis/blob/master/LICENCE).
