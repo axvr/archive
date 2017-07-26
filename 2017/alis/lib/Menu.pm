051488386b3c3944ff0111dfd12e42300a01a4f6
Alex Vear <axvr@users.noreply.github.com>
2017-07-26 18:46:57 +0100

    Comment out old keymap selection code

--- 2017/alis/lib/Menu.pm
+++ 2017/alis/lib/Menu.pm
@@ -255,101 +255,101 @@ sub pre_install_wipe_disks_yesno_screen {
 }
 
 
-sub keymap_selection_screen {
-    # TODO change this
-    sub keys1 {
-        $whiptail = qq{whiptail --title "Select keyboard layout" --radiolist --nocancel }.
-            qq{--backtitle "ARCH LINUX INSTALLATION SCRIPT $hardware_version $uefi_or_bios" }.
-            qq{"\nChoose Your Keyboard Layout:" 15 55 6 }.
-            qq{"QWERTY" "The QWERTY keymap (Most Common)" "ON" }.
-            qq{"QWERTZ" "The QWERTZ style keyboard layout" "OFF" }.
-            qq{"AZERTY" "The AZERTY keyboard layout" "OFF" }.
-            qq{"Dvorak" "The Dvorak keyoard layout map" "OFF" }.
-            qq{"Colemak" "The Colemak keyboard layout" "OFF" }.
-            qq{"Other" "Select a different keymap here" "OFF" 3>&1 1>&2 2>&3 };
-        $keyboard_layout = `$whiptail`;
-        $keyboard_layout =~ tr/A-Z/a-z/;
-
-        # other keymap selection menu
-        if ($keyboard_layout eq "other") {
-            $whiptail = qq{whiptail --title "Select keyboard layout" }.
-                qq{--backtitle "ARCH LINUX INSTALLATION SCRIPT $hardware_version $uefi_or_bios" }.
-                qq{--inputbox "\nInput a different keymap" 7 45 "" 3>&1 1>&2 2>&3};
-            my $keymap = `$whiptail`;
-
-            # check the keymap selection was not null
-            if ($keymap eq "") {
-                keys1();
-            } else {
-                # load the selected keymap
-                my @values = split(' ', $keymap);
-                $keymap = $values[0];
-                my $run_loadkeys = system("loadkeys", "$keymap");
-                print $fh "Loading keymap: $keymap\n";
-
-                # check if the keymap loading failed
-                if ($run_loadkeys != 0) {
-                    system("whiptail", "--title", "Invalid keymap", "--msgbox",
-                           "--backtitle", "ARCH LINUX INSTALLATION SCRIPT $hardware_version $uefi_or_bios",
-                           "Keymap '$keymap' was invalid. Press OK to go back.", "8", "45");
-                    print $fh "Keymap was invalid, starting again\n";
-                    keys1();
-                } else {
-                    print $fh "Keymap was valid, continuing\n";
-                    print $fh "Running main-menu.pl using perl\n";
-                    close $fh or die "Could not close '$log_file'. $!";
-                    system("whiptail", "--title", "Keymap was successfully set", "--msgbox",
-                           "--backtitle", "ARCH LINUX INSTALLATION SCRIPT $hardware_version $uefi_or_bios",
-                           "Keymap '$keymap' was set successfully. Press OK to continue.", "8", "45");
-                    exec("perl", "main-menu.pl", "$hardware_version", "$uefi_or_bios", "$keymap");
-                }
-            }
-        } elsif ($keyboard_layout ne "other") {
-            keys2();
-            # load the second section of the keymap selection if it was not custom set
-        }
-    }
-
-    # choose keymap part 2
-    sub keys2 {
-
-        # fetch and format the keymap config selection form the system
-        my @keymaps = `ls /usr/share/kbd/keymaps/i386/$keyboard_layout/`;
-        my $layouts_for_whiptail = "";
-
-        #foreach (my $i (0..($total - 1)) {
-        foreach (@keymaps) {
-            chomp($_);
-            if ($_ eq "us.map.gz") {
-                $layouts_for_whiptail = $layouts_for_whiptail . qq{"$_" "ON" };
-            } else {
-                $layouts_for_whiptail = $layouts_for_whiptail . qq{"$_" "OFF" };
-            }
-        }
-
-        # select the second section of the keyamp and store the result
-        $whiptail = qq{whiptail --title "Select keyboard layout" }.
-            qq{--radiolist --noitem --cancel-button "Back" }.
-            qq{--backtitle "ARCH LINUX INSTALLATION SCRIPT $hardware_version $uefi_or_bios" }.
-            qq{"\nChoose Your Keyboard Layout:  (Default = US QWERTY)" 15 55 7 }.
-            qq{$layouts_for_whiptail}.
-            qq{3>&1 1>&2 2>&3 };
-        my $keymap = `$whiptail`;
-        if ($keymap eq "") {
-            keys1();
-        } else {
-            # load the new keymap and the main-menu.pl file with paramters
-            print $fh "Loading keymap: $keyboard_layout/$keymap\n";
-            system("loadkeys", "$keyboard_layout/$keymap");
-            # run the menu script (parameters to send the architecture, boot mode, and keyboard layout)
-            print $fh "\nRunning main-menu.pl using perl\n";
-            close $fh or die "Could not close '$log_file'. $!";
-            exec("perl", "main-menu.pl", "$hardware_version", "$uefi_or_bios", "$keymap");
-        }
-    }
-
-    keys1();;
-}
+#sub keymap_selection_screen {
+#    # TODO change this
+#    sub keys1 {
+#        $whiptail = qq{whiptail --title "Select keyboard layout" --radiolist --nocancel }.
+#            qq{--backtitle "ARCH LINUX INSTALLATION SCRIPT $hardware_version $uefi_or_bios" }.
+#            qq{"\nChoose Your Keyboard Layout:" 15 55 6 }.
+#            qq{"QWERTY" "The QWERTY keymap (Most Common)" "ON" }.
+#            qq{"QWERTZ" "The QWERTZ style keyboard layout" "OFF" }.
+#            qq{"AZERTY" "The AZERTY keyboard layout" "OFF" }.
+#            qq{"Dvorak" "The Dvorak keyoard layout map" "OFF" }.
+#            qq{"Colemak" "The Colemak keyboard layout" "OFF" }.
+#            qq{"Other" "Select a diffe#rent keymap here" "OFF" 3>&1 1>&2 2>&3 };
+#        $keyboard_layout = `$whiptail`;
+#        $keyboard_layout =~ tr/A-Z/a-z/;
+#
+#        # other keymap selection menu
+#        if ($keyboard_layout eq "other") {
+#            $whiptail = qq{whiptail --title "Select keyboard layout" }.
+#                qq{--backtitle "ARCH LINUX INSTALLATION SCRIPT $hardware_version $uefi_or_bios" }.
+#                qq{--inputbox "\nInput a different keymap" 7 45 "" 3>&1 1>&2 2>&3};
+#            my $keymap = `$whiptail`;
+#
+#            # check the keymap selection was not null
+#            if ($keymap eq "") {
+#                keys1();
+#            } else {
+#                # load the selected keymap
+#                my @values = split(' ', $keymap);
+#                $keymap = $values[0];
+#                my $run_loadkeys = system("loadkeys", "$keymap");
+#                print $fh "Loading keymap: $keymap\n";
+#
+#                # check if the keymap loading failed
+#                if ($run_loadkeys != 0) {
+#                    system("whiptail", "--title", "Invalid keymap", "--msgbox",
+#                           "--backtitle", "ARCH LINUX INSTALLATION SCRIPT $hardware_version $uefi_or_bios",
+#                           "Keymap '$keymap' was invalid. Press OK to go back.", "8", "45");
+#                    print $fh "Keymap was invalid, starting again\n";
+#                    keys1();
+#                } else {
+#                    print $fh "Keymap was valid, continuing\n";
+#                    print $fh "Running main-menu.pl using perl\n";
+#                    close $fh or die "Could not close '$log_file'. $!";
+#                   system("whiptail", "--title", "Keymap was successfully set", "--msgbox",
+#                           "--backtitle", "ARCH LINUX INSTALLATION SCRIPT $hardware_version $uefi_or_bios",
+#                           "Keymap '$keymap' was set successfully. Press OK to continue.", "8", "45");
+#                    exec("perl", "main-menu.pl", "$hardware_version", "$uefi_or_bios", "$keymap");
+#                }
+#            }
+#        } elsif ($keyboard_layout ne "other") {
+#            keys2();
+#            # load the second section of the keymap selection if it was not custom set
+#        }
+#    }
+#
+#    # choose keymap part 2
+#    sub keys2 {
+#
+#        # fetch and format the keymap config selection form the system
+#        my @keymaps = `ls /usr/share/kbd/keymaps/i386/$keyboard_layout/`;
+#        my $layouts_for_whiptail = "";
+#
+#        #foreach (my $i (0..($total - 1)) {
+#        foreach (@keymaps) {
+#            chomp($_);
+#            if ($_ eq "us.map.gz") {
+#               $layouts_for_whiptail = $layouts_for_whiptail . qq{"$_" "ON" };
+#            } else {
+#                $layouts_for_whiptail = $layouts_for_whiptail . qq{"$_" "OFF" };
+#            }
+#        }
+#
+#        # select the second section of the keyamp and store the result
+#        $whiptail = qq{whiptail --title "Select keyboard layout" }.
+#            qq{--radiolist --noitem --cancel-button "Back" }.
+#            qq{--backtitle "ARCH LINUX INSTALLATION SCRIPT $hardware_version $uefi_or_bios" }.
+#            qq{"\nChoose Your Keyboard Layout:  (Default = US QWERTY)" 15 55 7 }.
+#            qq{$layouts_for_whiptail}.
+#            qq{3>&1 1>&2 2>&3 };
+#        my $keymap = `$whiptail`;
+#        if ($keymap eq "") {
+#            keys1();
+#        } else {
+#            # load the new keymap and the main-menu.pl file with paramters
+#            print $fh "Loading keymap: $keyboard_layout/$keymap\n";
+#            system("loadkeys", "$keyboard_layout/$keymap");
+#            # run the menu script (parameters to send the architecture, boot mode, and keyboard layout)
+#            print $fh "\nRunning main-menu.pl using perl\n";
+#            close $fh or die "Could not close '$log_file'. $!";
+#            exec("perl", "main-menu.pl", "$hardware_version", "$uefi_or_bios", "$keymap");
+#        }
+#    }
+#
+#    keys1();;
+#}
 
 
 1;
