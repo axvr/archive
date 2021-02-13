9b74f5417a8fa5016eed5494ae193e3e37ae610d
Alex Vear <av@axvr.io>
2021-02-13 17:29:44 +0000

    Wrap formal parameters across multiple lines

--- 2020/LinqLisp/LinqLisp/Conditionals/Unless.cs
+++ 2020/LinqLisp/LinqLisp/Conditionals/Unless.cs
@@ -6,36 +6,54 @@ namespace LinqLisp
 {
     public static partial class Conditionals
     {
-        public static IEnumerable<TSource> Unless<TSource>(this IEnumerable<TSource> source, bool cond, Func<IEnumerable<TSource>, IEnumerable<TSource>> then)
+        public static IEnumerable<TSource> Unless<TSource>(
+                this IEnumerable<TSource> source,
+                bool cond,
+                Func<IEnumerable<TSource>, IEnumerable<TSource>> then)
         {
             return source.When(! cond, then);
         }
 
-        public static IOrderedEnumerable<TSource> Unless<TSource>(this IOrderedEnumerable<TSource> source, bool cond, Func<IOrderedEnumerable<TSource>, IOrderedEnumerable<TSource>> then)
+        public static IOrderedEnumerable<TSource> Unless<TSource>(
+                this IOrderedEnumerable<TSource> source,
+                bool cond,
+                Func<IOrderedEnumerable<TSource>, IOrderedEnumerable<TSource>> then)
         {
             return source.When(! cond, then);
         }
 
 
 
-        public static IEnumerable<TSource> Unless<TSource>(this IEnumerable<TSource> source, Func<IEnumerable<TSource>, bool> predicate, Func<IEnumerable<TSource>, IEnumerable<TSource>> then)
+        public static IEnumerable<TSource> Unless<TSource>(
+                this IEnumerable<TSource> source,
+                Func<IEnumerable<TSource>, bool> predicate,
+                Func<IEnumerable<TSource>, IEnumerable<TSource>> then)
         {
             return source.When(! predicate(source), then);
         }
 
-        public static IOrderedEnumerable<TSource> Unless<TSource>(this IOrderedEnumerable<TSource> source, Func<IEnumerable<TSource>, bool> predicate, Func<IOrderedEnumerable<TSource>, IOrderedEnumerable<TSource>> then)
+        public static IOrderedEnumerable<TSource> Unless<TSource>(
+                this IOrderedEnumerable<TSource> source,
+                Func<IEnumerable<TSource>, bool> predicate,
+                Func<IOrderedEnumerable<TSource>, IOrderedEnumerable<TSource>> then)
         {
             return source.When(! predicate(source), then);
         }
 
 
 
-        public static IQueryable<TSource> Unless<TSource>(this IQueryable<TSource> source, bool cond, Func<IQueryable<TSource>, IQueryable<TSource>> then)
+        public static IQueryable<TSource> Unless<TSource>(
+                this IQueryable<TSource> source,
+                bool cond,
+                Func<IQueryable<TSource>, IQueryable<TSource>> then)
         {
             return source.When(! cond, then);
         }
 
-        public static IOrderedQueryable<TSource> Unless<TSource>(this IOrderedQueryable<TSource> source, bool cond, Func<IOrderedQueryable<TSource>, IOrderedQueryable<TSource>> then)
+        public static IOrderedQueryable<TSource> Unless<TSource>(
+                this IOrderedQueryable<TSource> source,
+                bool cond,
+                Func<IOrderedQueryable<TSource>, IOrderedQueryable<TSource>> then)
         {
             return source.When(! cond, then);
         }
--- 2020/LinqLisp/LinqLisp/Conditionals/When.cs
+++ 2020/LinqLisp/LinqLisp/Conditionals/When.cs
@@ -6,36 +6,54 @@ namespace LinqLisp
 {
     public static partial class Conditionals
     {
-        public static IEnumerable<TSource> When<TSource>(this IEnumerable<TSource> source, bool cond, Func<IEnumerable<TSource>, IEnumerable<TSource>> then)
+        public static IEnumerable<TSource> When<TSource>(
+                this IEnumerable<TSource> source,
+                bool cond,
+                Func<IEnumerable<TSource>, IEnumerable<TSource>> then)
         {
             return cond ? then(source) : source;
         }
 
-        public static IOrderedEnumerable<TSource> When<TSource>(this IOrderedEnumerable<TSource> source, bool cond, Func<IOrderedEnumerable<TSource>, IOrderedEnumerable<TSource>> then)
+        public static IOrderedEnumerable<TSource> When<TSource>(
+                this IOrderedEnumerable<TSource> source,
+                bool cond,
+                Func<IOrderedEnumerable<TSource>, IOrderedEnumerable<TSource>> then)
         {
             return cond ? then(source) : source;
         }
 
 
 
-        public static IEnumerable<TSource> When<TSource>(this IEnumerable<TSource> source, Func<IEnumerable<TSource>, bool> predicate, Func<IEnumerable<TSource>, IEnumerable<TSource>> then)
+        public static IEnumerable<TSource> When<TSource>(
+                this IEnumerable<TSource> source,
+                Func<IEnumerable<TSource>, bool> predicate,
+                Func<IEnumerable<TSource>, IEnumerable<TSource>> then)
         {
             return predicate(source) ? then(source) : source;
         }
 
-        public static IOrderedEnumerable<TSource> When<TSource>(this IOrderedEnumerable<TSource> source, Func<IOrderedEnumerable<TSource>, bool> predicate, Func<IOrderedEnumerable<TSource>, IOrderedEnumerable<TSource>> then)
+        public static IOrderedEnumerable<TSource> When<TSource>(
+                this IOrderedEnumerable<TSource> source,
+                Func<IOrderedEnumerable<TSource>, bool> predicate,
+                Func<IOrderedEnumerable<TSource>, IOrderedEnumerable<TSource>> then)
         {
             return predicate(source) ? then(source) : source;
         }
 
 
 
-        public static IQueryable<TSource> When<TSource>(this IQueryable<TSource> source, bool cond, Func<IQueryable<TSource>, IQueryable<TSource>> then)
+        public static IQueryable<TSource> When<TSource>(
+                this IQueryable<TSource> source,
+                bool cond,
+                Func<IQueryable<TSource>, IQueryable<TSource>> then)
         {
             return cond ? then(source) : source;
         }
 
-        public static IOrderedQueryable<TSource> When<TSource>(this IOrderedQueryable<TSource> source, bool cond, Func<IOrderedQueryable<TSource>, IOrderedQueryable<TSource>> then)
+        public static IOrderedQueryable<TSource> When<TSource>(
+                this IOrderedQueryable<TSource> source,
+                bool cond,
+                Func<IOrderedQueryable<TSource>, IOrderedQueryable<TSource>> then)
         {
             return cond ? then(source) : source;
         }
