d1bfcde98f2986c5c7400df9133cc6173fefa34a
Alex Vear <alex@vear.uk>
2022-02-05 20:19:44 +0000

    Initial version

---
+++ 2022/dirigo/README.md
@@ -0,0 +1,6 @@
+# Web forwarding service
+
+Powers \*.axvr.uk.
+
+For subdomains, add a CNAME record to `axvr.uk.`  For root domain redirections,
+add an A record pointing to `...`.
---
+++ 2022/dirigo/src/uk/axvr/redirect/core.clj
@@ -0,0 +1,30 @@
+(ns uk.axvr.redirect.core
+  (:use clojure.pprint)
+  (:require [clojure.java.io :as io]
+            [clojure.edn :as edn]
+            [ring.adapter.jetty :refer [run-jetty]]))
+
+(defn read-edn-resource [f]
+  (some-> f io/resource slurp edn/read-string))
+
+(defonce redirections
+  (atom (read-edn-resource "redirections.edn")))
+
+(defn redirect [request]
+  (pprint request)
+  (let [host (str (name (:scheme request)) "://" (get-in request [:headers "host"]))
+        loc  (get @redirections host)]
+    {:status 301
+     :headers {"Location" loc}}))
+
+(defn app-handler [request]
+  (redirect request))
+
+(defn run [{:keys [port block?]
+            :or   {port   80
+                   block? true}}]
+  (run-jetty
+    #'app-handler
+    {:port   port
+     :join?  (not block?)
+     :send-server-version? false}))
