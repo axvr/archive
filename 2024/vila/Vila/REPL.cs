2b273f3ec52ffd80b36dc390dbe1fd7f96c81576
Alex Vear <alex@vear.uk>
2025-07-08 14:49:39 +0100

    Use the `.il` file extension for CIL files

--- 2024/vila/Vila/REPL.cs
+++ 2024/vila/Vila/REPL.cs
@@ -6,7 +6,7 @@ internal class Program
 {
     public static void Main()
     {
-        Uri f = new("file:///Users/axvr/Projects/Experiments/vila/Examples/hello.cil");
+        Uri f = new("file:///Users/axvr/Projects/Experiments/vila/Examples/hello.il");
         Console.WriteLine(f.AbsolutePath);
         using var sr = Reader.UriToStreamReader(f);
         foreach (var token in Reader.ReadTokens(sr, f)) Console.WriteLine(token);
